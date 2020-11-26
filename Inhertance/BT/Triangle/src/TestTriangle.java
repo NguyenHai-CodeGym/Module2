79a462cb9505a6ce315eee67b96db72726f8eaba
Hai.nguyen <ng.hai0603@gmail.com>
2020-11-26 17:20:31 +0700

    up

--- Inhertance/BT/Triangle/src/TestTriangle.java
+++ Inhertance/BT/Triangle/src/TestTriangle.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class TestTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Color : ");
+        String a = sc.nextLine();
         System.out.println("Side 1: ");
-        double a = sc.nextDouble();
-        System.out.println("Side 2: ");
         double b = sc.nextDouble();
-        System.out.println("Side 3: ");
+        System.out.println("Side 2: ");
         double c = sc.nextDouble();
-        Triangle triangle = new Triangle("blue",a,b,c);
+        System.out.println("Side 3: ");
+        double d = sc.nextDouble();
+        Triangle triangle = new Triangle(a,b,c,d);
         System.out.println(triangle);
         System.out.println("Chu Vi Tam Giac: " + triangle.getPerimeter());
         System.out.println("Dien Tich Tam Giac: " + triangle.getAre());
