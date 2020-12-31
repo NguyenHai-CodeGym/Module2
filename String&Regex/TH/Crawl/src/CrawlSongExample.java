9d7cb3f833caaa63ad0a0e4f1d26b3c869dacc43
Hai.nguyen <ng.hai0603@gmail.com>
2020-12-31 13:14:55 +0700

    up

---
+++ String&Regex/TH/Crawl/src/CrawlSongExample.java
@@ -0,0 +1,26 @@
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlSongExample {
+    public static void main(String[] args) {
+        try{
+            URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
+            Scanner sc = new Scanner(new InputStreamReader(url.openStream()));
+            sc.useDelimiter("\\Z");
+            String content = sc.next();
+            sc.close();
+            content = content.replaceAll("\\n+", "");
+            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
+            Matcher matcher = p.matcher(content);
+            while (matcher.find()){
+                System.out.println(matcher.group(1));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
